955aa5689437f616b669c6dbb976e711f5a4efc5
Hitmic <omkarkalyana2072@gmail.com>
2026-05-26 17:16:18 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 from sklearn.preprocessing import MinMaxScaler
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import streamlit as st
  
 # ============================================================
